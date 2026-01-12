2785ccf42cf2252e7a1e4a9bd47755e62edd8199
thisnabeel <nabeel.khan.swe@gmail.com>
2026-01-12 02:14:59 -0800

    wizard

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -5,7 +5,7 @@ class Experiment < ApplicationRecord
   validates :title, presence: true
 
   def self.generate_for_concept(concept)
-    prompt = "Generate a thought experiment for #{concept.title}. Return json format: {title: \"the title of the thought experiment\", body: \"the body content of the thought experiment with HTML formatting for paragraphs and emphasis\"}"
+    prompt = "Generate a thought experiment for one of the philosophies of #{concept.title}. It should be bullet point digestible and minimal sized formatting. Max 300 characters. Return json format: {title: \"the title of the thought experiment\", body: \"the body content of the thought experiment with HTML formatting for paragraphs and emphasis\"}"
     
     res = ChatGpt.send(prompt)
     
